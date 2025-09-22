1ff28c3afb3a34719026571454bdf6557e96afec
jn1za <zarajc@hawaii.edu>
2025-09-21 20:05:35 -1000

    Update ziplist.ts

--- ziplist.ts
+++ ziplist.ts
@@ -1,2 +1,12 @@
-console.log('who are you?');
-console.log('I am fine, thank you.');
+function zipList(list1: unknown[], list2: unknown[]): unknown[] {
+  const result = [];
+  for (let i = 0; i < list1.length; i++) {
+    result.push(list1[i], list2[i]);
+  }
+  return result;
+}
+
+const LIST1 = ['a', 'b', 'c'];
+const LIST2 = [1, 2, 3];
+
+console.log(zipList(LIST1, LIST2)); // ['a', 1, 'b', 2, 'c', 3]
