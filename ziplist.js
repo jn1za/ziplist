c8151666ac932c98aa58f77d49a3283c579733f9
jn1za <zarajc@hawaii.edu>
2025-09-21 20:06:17 -1000

    Update ziplist.js

--- ziplist.js
+++ ziplist.js
@@ -1,3 +1,11 @@
 "use strict";
-console.log('who are you?');
-console.log('I am fine, thank you.');
+function zipList(list1, list2) {
+    const result = [];
+    for (let i = 0; i < list1.length; i++) {
+        result.push(list1[i], list2[i]);
+    }
+    return result;
+}
+const LIST1 = ['a', 'b', 'c'];
+const LIST2 = [1, 2, 3];
+console.log(zipList(LIST1, LIST2)); // ['a', 1, 'b', 2, 'c', 3]
